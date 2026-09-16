a3c6c13a527ecfc66c545f481c8b43866303f075
Marcus Torres <marcuslorenlopes@gmail.com>
2026-09-16 19:01:40 -0300

    chore: limpeza inicial do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Stars Finder",
+  description: "Explore o GitHub de um jeito simples.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
