2c9291196e99db35f5762aef41f20923aed09407
Marcus Torres <marcuslorenlopes@gmail.com>
2026-09-17 15:47:55 -0300

    style: setup global para tipografia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,28 @@
 import type { Metadata } from "next";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import { Inter } from "next/font/google";
+import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "Stars Finder",
   description: "Explore o GitHub de um jeito simples.",
 };
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "400", "700", "800", "900"],
+  style: ["normal", "italic"],
+  variable: "--font-inter",
+});
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
