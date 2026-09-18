d3ef968797c65836e55ad29d5565be0d50669938
Marcus Torres <marcuslorenlopes@gmail.com>
2026-09-17 21:41:08 -0300

    feat: adicionado hero banner

---
+++ src/app/components/HeroBanner.tsx
@@ -0,0 +1,30 @@
+export default function HeroBanner() {
+  return (
+    <section className="hero-banner d-flex flex-column justify-content-center">
+      <div className="container text-center">
+        <h1 className="font-extralight mx-auto">
+          Explore o <span className="font-black">GitHub</span> de um
+          <span className="font-black mx-3">jeito</span>
+          <span className="font-black hero-highlight">simples.</span>
+        </h1>
+        <p className="mt-5 mb-4">
+          Encontre usuários, descubra seus repositórios e veja seus projetos
+          mais populares.
+        </p>
+        <button
+          type="button"
+          className="btn btn-dark button-cta d-inline-flex align-items-center justify-content-center gap-2"
+        >
+          encontre agora um usuário
+          <img
+            src="/images/icon-arrow-cta.svg"
+            alt="Ícone para ação de Buscar Usuário"
+            aria-hidden="true"
+            width={20}
+            height={20}
+          />
+        </button>
+      </div>
+    </section>
+  );
+}
