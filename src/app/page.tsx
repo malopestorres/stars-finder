26935dd9ed7aa33668106ab75895728e3e374ab7
Marcus Torres <marcuslorenlopes@gmail.com>
2026-09-17 23:03:16 -0300

    feat: adicionado main content da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import HomeContent from "@/components/HomeContent";
 import HeroBanner from "../components/HeroBanner";
 
 export default function Home() {
-  return <HeroBanner />;
+  return (
+    <>
+      <HeroBanner />
+      <HomeContent />
+    </>
+  );
 }
