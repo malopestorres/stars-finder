dcc32717e8ce7d23fdd77bdf7f32d2b4407ecd9a
Marcus Torres <marcuslorenlopes@gmail.com>
2026-09-17 22:00:45 -0300

    movido pasta componentes pra fora da pasta app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
-import Header from "./components/Header";
+import Header from "../components/Header";
 
 export const metadata: Metadata = {
   title: "Stars Finder",
