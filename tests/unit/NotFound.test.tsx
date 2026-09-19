91aae0492242baaca445b285f5c3848c3f30fd11
Marcus Torres <marcuslorenlopes@gmail.com>
2026-09-19 14:52:37 -0300

    test: adiciona teste de rota não encontrada

---
+++ tests/unit/NotFound.test.tsx
@@ -0,0 +1,25 @@
+// @vitest-environment jsdom
+
+import { cleanup, render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest";
+import NotFound from "../../src/app/not-found";
+
+describe("NotFound", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("deve renderizar o ícone, o texto e o botão de voltar", () => {
+    render(<NotFound />);
+
+    expect(
+      screen.getByText("oops! o que você está procurando não existe."),
+    ).toBeTruthy();
+
+    const backButton = screen.getByRole("link", { name: "Voltar" });
+    expect(backButton.getAttribute("href")).toBe("/");
+
+    const icon = screen.getByRole("img", { hidden: true });
+    expect(icon.getAttribute("src")).toBe("/images/icon-not-found.svg");
+  });
+});
